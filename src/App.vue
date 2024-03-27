6c496b95bcc8c71e6c5c7b07c3beb6697b070c53
LAPTOP-SB0CM2VU\Playdata <wjdxodndjaak@naver.com>
2024-03-27 16:04:32 +0900

    누락파일 재업로드

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-    <RouterView/>
+    <!-- <RouterView/> -->
+    <!-- <ProjectMember/> -->
+    <ProjectList/>
+    
+    
 </template>
 
 <script setup>
+    import ProjectMember from '@/views/projectMember/ProjectMember.vue'
     import { RouterView } from 'vue-router';
+    import ProjectList from '@/views/projectList/ProjectList.vue'
 </script>
 
 <style scoped>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/views/main/Home.vue';
 import SignIn from '@/views/SignIn.vue';
 import SignUp from '@/views/SignUp.vue';
+import ProjectInfo from '@/views/projectView/ProjectInfo.vue';
+import ProjectMemberInfo from '@/views/projectView/ProjectMemberInfo.vue';
+
 
 
 const router = createRouter({
@@ -18,6 +21,16 @@ const router = createRouter({
       { 
         path: '/signup', 
         component: SignUp 
+      },
+      {
+        path: '/ProjectMemberInfo',
+        component: ProjectMemberInfo
+        
+      },
+      {
+          path: '/ProjectInfo',
+          component: ProjectInfo
+
       }
     ]
 });
