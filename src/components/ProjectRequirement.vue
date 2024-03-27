96d192de91f05ee79b3ffb03808742e14f458f37
oneye <onelee521@gmail.com>
2024-03-27 19:38:20 +0900

    feat(Requirement): json 파일로 요구사항 조회, 추가, 삭제
    json 파일로 생성함

---
+++ src/components/ProjectRequirement.vue
@@ -0,0 +1,157 @@
+<template>
+    <div class="container">
+        <Header :projectName="projectName"></Header>
+        <div class="table-responsive">
+            <table class="table table-bordered">
+            <thead>
+                <tr>
+                <th v-for="field in fields" :key="field.key">{{ field.label }}</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(row, index) in tableData" :key="row.no">
+                <td> {{ row.no }} </td>
+                <td>
+                    <input
+                    v-if="!isReadOnly"
+                    v-model="row.separate"
+                    type="text"
+                    class="form-control"
+                    placeholder="분류"
+                    />
+                    <span v-else>{{ row.separate }}</span>
+                </td>
+                <td>
+                    <input
+                    v-if="!isReadOnly"
+                    v-model="row.name"
+                    type="text"
+                    class="form-control"
+                    placeholder="요구사항 명"
+                    />
+                    <span v-else>{{ row.name }}</span>
+                </td>
+                <td>
+                    <input
+                    v-if="!isReadOnly"
+                    v-model="row.content"
+                    type="text"
+                    class="form-control"
+                    placeholder="요구사항 내용"
+                    />
+                    <span v-else>{{ row.content }}</span>
+                </td>
+                <td>
+                    <input
+                    v-if="!isReadOnly"
+                    v-model="row.note"
+                    type="text"
+                    class="form-control"
+                    placeholder="비고"
+                    />
+                    <span v-else>{{ row.note }}</span>
+                </td>
+                <td v-if="!isReadOnly">
+                    <button class="btn btn-dark" @click="deleteRow(index)">DELETE</button>
+                </td>
+                </tr>
+            </tbody>
+            </table>
+        </div>
+        <div class="d-flex justify-content-between mt-3">
+            <button class="btn btn-dark" @click="backToProject">Back to Project</button>
+            <div>
+            <button v-if="!isReadOnly" class="btn btn-dark me-2" @click="isReadOnly = true">Save</button>
+            <button v-if="!isReadOnly" class="btn btn-dark" @click="addRow()">Add Column</button>
+            <button v-else class="btn btn-dark" @click="isReadOnly = false">Modify</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import Header from './ProjectTemplateHeader.vue'
+    import { onMounted, ref } from 'vue'
+    import requirementdummy from '@/testjson/requirementdummy.json'
+
+    const tableData = ref([]);
+    
+    // 현재 json 파일로 불러옴 서버로 변경 필요
+    onMounted(() => {
+        tableData.value = requirementdummy.requirementVOList;
+    });
+    
+    const fields = ref([
+        { key: 'no', label: 'No'},
+        { key: 'separate', label: '분류' },
+        { key: 'name', label: '요구사항 명'},
+        { key: 'content', label: '요구사항 내용' },
+        { key: 'note', label: '비고' }
+    ])
+    
+    // 페이지에 들어왔을 때 modify 버튼 활성화
+    const isReadOnly = ref(true)
+    
+    const addRow = () => {
+        const index = tableData.value.length;
+        tableData.value.push({ no: index+1, separate: '', name: '', content: '', note: '' })
+    }
+    
+    const deleteRow = (index) => {
+        tableData.value.splice(index, 1);
+
+        // 삭제 후 인덱스 값 업데이트(서버랑 연결되면 변경될 수 있음)
+        for (let i = index; i < tableData.value.length; i++) {
+            tableData.value[i].no = i+1;
+        }
+    }
+    
+    const backToProject = () => {
+        // Back to Project 버튼 클릭 시 동작할 내용 작성
+        console.log('Back to Project clicked')
+    }
+</script>
+
+<style scoped>
+
+    .table-responsive {
+        overflow-x: auto; /* 가로 스크롤을 필요로 하는 경우 */
+        overflow-y: auto; /* 세로 스크롤을 필요로 하는 경우 */
+        max-height: 1000px; /* 원하는 높이로 설정 */
+    }
+    .table {
+        width: 100%;
+    }
+    
+    .table th,
+    .table td {
+        padding: 0.5rem;
+        text-align: center;
+    }
+    
+    
+    .table th:nth-child(5),
+    .table td:nth-child(5)
+    {
+        min-width: 120px;
+        width: 30%;
+    }
+    
+    
+    
+    .table th:nth-child(4),
+    .table td:nth-child(4)
+    {
+        min-width: 200px;
+        width: 40%;
+    }
+
+    .table th:nth-child(2),
+    .table td:nth-child(2),
+    .table th:nth-child(3),
+    .table td:nth-child(3)
+    {
+        min-width: 150px;
+        width: 20%;
+    }
+</style>
