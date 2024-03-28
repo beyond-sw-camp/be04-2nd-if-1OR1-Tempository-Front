7a4762a2c5e88cf5362492b97f526db430b63350
oneye <onelee521@gmail.com>
2024-03-28 13:56:58 +0900

    refactor(Testcase): back to project 주소 변경
    라우터 주소 변경함

--- src/components/ProjectTestcase.vue
+++ src/components/ProjectTestcase.vue
@@ -156,8 +156,7 @@
     
     function backToProject() {
 
-        // projectInfo 페이지로 이동(프로젝트id 추가 필요)
-        router.push('/projectInfo');
+        router.push(`/project/${currentRoute.params.id}`);
     }
 </script>
 
